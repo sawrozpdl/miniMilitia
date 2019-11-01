e60c76f8f9644f1f03b42fa9849ae0e39f7ae65e
Saroj <sarojpaudyal53@gmail.com>
2019-11-01 08:49:20 +0545

    fixing clones

--- js/network/Connection.js
+++
@@ -1,3 +0,0 @@
-import Peer from './peer.js';
-
-
--- js/script.js
+++
@@ -1,8 +0,0 @@
-import Main from './main.js';
-
-var canvas = document.getElementById('game-container');
-var game = new Main(canvas);
-game.loadAssets();
-canvas.addEventListener('click', (e) => {
-    if (!game.gameStarted) game.startGame(e);
-});
